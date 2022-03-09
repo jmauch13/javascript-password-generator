c044b3408815cf4a9ba148ac0b492741a3c7d4ca
Jennie Mauch <jenniemauch@me.com>
2022-03-09 09:28:49 -0500

    let statements added

--- assets/password.js
+++ assets/password.js
@@ -1,4 +1,59 @@
 // Assignment code here
+// const bigLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// console.log(bigLetters.toUpperCase());
+
+// const littleLetters = "abcdefghijklmnopqrstuvwxyz";
+// console.log(littleLetters.toLowerCase());
+
+
+// var specialSymbols ="!@#$%^&*()_+";
+
+
+
+  let number = prompt("Enter your password length.", "between 8-128 characters");
+  if (numbers = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
+  26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45,
+  46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65,
+  66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85,
+  86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104,
+  105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120,
+  121, 122, 123, 124, 125, 126, 127, 128]);
+  let min = Math.min( ...numbers )
+  // min is 8
+  let max = Math.max( ...numbers )
+  // max is 128
+  console.log("min = " + min);
+  console.log("max = " + max);
+  
+
+  let bigL = prompt("Would you like to include uppercase letters?", "yes or no");
+  if (yes = (""));
+  else alertcd("Error.  Please confirm yes or no.");
+  let bigLetters = "A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z";
+  console.log(bigLetters.toUpperCase());
+
+  let littleL = prompt("Would you like to include lowercase letters?", "yes or no");
+  let y = ("");
+  let n = ("");
+  let littleLetters = "a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z";
+  console.log(littleLetters.toLowerCase());
+
+  let special = prompt("Would you like to include special characters?", "yes or no");
+  let positive = ("");
+  let negative = ("");
+  let specialChar = "!, @, #, $, %, ^, &, *, (, ), _, +, =,";
+  console.log("");
+  
+  // if (passwordLength < 8 || passwordLength > 128) {
+    //  return;
+   //  } else {
+   //  ("Not a valid number.  Please choose a number between 1-128.");
+   // }
+   // return;
+//  console.log(passwordLength);
+ // }); 
+
+
 
 
 // Get references to the #generate element
